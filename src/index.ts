a2f3d961da6ecc9c889b8d251d40b05b20a719de
TreeSome <youn@tree-some.dev>
2021-03-13 02:17:59 +0900

    picker

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { AES } from 'crypto-js';
 import { parse as CookieParse } from 'tough-cookie';
 import got from 'got';
 
+import fs from 'fs';
+
 export class KakaoLink {
 
 	private readonly SHARER_LINK: string = 'https://sharer.kakao.com';
@@ -13,12 +15,22 @@ export class KakaoLink {
 		headers: {
 			'user-agent': this.userAgent,
 		},
+		hooks: {
+			beforeError: [
+				(error: any) => {
+					const { response } = error;
+					console.log(error, response.body);
+					return error;
+				},
+			],
+		},
 	});
 
 	private SDK_INFO: string = '';
 	private p: string = '';
 	private continue: string = '';
 	private cookie: Record<string, unknown> = {};
+	private ver: string = '4.0';
 
 	constructor(private key: string, host: string, private userAgent: string = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36') {
 		this.SDK_INFO = [
@@ -29,6 +41,19 @@ export class KakaoLink {
 			`device/Linux_x86_64`,
 			`origin/${qs.escape(host)}`,
 		].join(' ');
+	}
+
+	get version() {
+		return this.ver;
+	}
+
+	set version(val: string) {
+		this.ver = val;
+	}
+
+	private validateEmail(email: string) {
+		const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email.toLowerCase());
 	}
 
 	private enc(text: string) {
@@ -53,8 +78,19 @@ export class KakaoLink {
 		return cook;
 	}
 
-	private cook() {
-		return Object.entries(this.cookie).map((ck: any) => `${ck[0]}=${ck[1].value}`).join('; ') + ';';
+	private cook(filter?: string[]) {
+		const entries = Object.entries(this.cookie);
+		const cookie: string[] = [];
+		for ( const [key, val] of entries ){
+			if ( filter ) {
+				if ( filter.includes(key) ) {
+					cookie.push(`${key}=${(val as any).value}`);
+				}
+			} else {
+				cookie.push(`${key}=${(val as any).value}`);
+			}
+		}
+		return cookie.join('; ') + ';';
 	}
 
 	private cooking(cook: Record<string, unknown>, keys: string[]) {
@@ -73,15 +109,34 @@ export class KakaoLink {
 		return res;
 	}
 
-	async login(email: string, pw: string) {
-		let res: any;
-		res = await this.req('POST',`${this.SHARER_LINK}/talk/friends/picker/link`,{
+	private async picker(action: string = 'default', params: Record<string, unknown> = {}) {
+		const opt: Record<string, unknown> = {};
+
+		const cookie = this.cook([ '_kadu', 'TIARA', '_kawlt', '_kawltea', '_karmt', '_karmtea' ]);
+		if ( cookie && this.continue ) {
+			opt['headers'] = {
+				'referer': this.continue,
+				'Cookie': cookie,
+			};
+		}
+
+		return await this.req('POST',`${this.SHARER_LINK}/talk/friends/picker/link`, {
 			'app_key': this.key,
-			'validation_action': 'default',
-			'validation_params': '{}',
+			'validation_action': action,
+			'validation_params': JSON.stringify(params),
 			'ka': this.SDK_INFO,
 			'lcba': '',
-		});
+		}, opt);
+	}
+
+	public async login(email: string, pw: string) {
+		let res: any;
+
+		if ( !this.validateEmail(email) ) {
+			email += '@kakao.com';
+		}
+
+		res = await this.picker();
 
 		const ckey = res.data.match(/<input.*?name="p".*?value="(.*?)".*?>/m)[1];
 		const token = res.data.match(/<meta.*?name="csrf-token".*?content="(.*?)".*?>/m)[1];
@@ -111,6 +166,18 @@ export class KakaoLink {
 		});
 
 		this.cooking(res.cookies, [ '_kawlt', '_kawltea', '_karmt', '_karmtea' ]);
+
+		return this;
+	}
+
+	public async send(room_title: string, template: Record<string, unknown>) {
+		let res: any;
+
+		if ( !template['link_ver'] ) {
+			template['link_ver'] = this.version;
+		}
+
+		res = await this.picker('custom', template);
 	}
 
 }
